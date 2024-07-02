38874a21752bce64d02209e7a4753eeb556bd43f
Gia Khang <giakhangksd@gmail.com>
2024-07-02 20:48:41 +0700

    add 2 loading intro scene+ create invisible obj for koopa

---
+++ 04-Collision/InvisibleObject.h
@@ -0,0 +1,35 @@
+#include "GameObject.h"
+#include "debug.h"
+
+#define INVISIBLE_BBOX_WIDTH				10
+#define INVISIBLE_BBOX_HEIGHT				500
+
+class CInvisibleObject : public CGameObject {
+	bool isActived;
+
+public:
+	CInvisibleObject(float x, float y) : CGameObject(x, y) {
+		isActived = false;
+	}
+
+	void Render() {
+		//RenderBoundingBox(); 
+	}
+
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+
+	int IsBlocking() {
+		return 0;
+	}
+
+	void Activating() {
+		isActived = true;
+	}
+
+	bool IsActived() {
+		//DebugOut(L"isActived: %d\n", isActived); 
+		return isActived;
+	}
+};
+
