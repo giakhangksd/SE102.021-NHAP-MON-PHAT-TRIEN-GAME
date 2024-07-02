e93dc58bd04e5b22787a306754cad39429b30e72
Gia Khang <giakhangksd@gmail.com>
2024-07-02 09:14:04 +0700

    test loadscene

--- 04-Collision/Mario.cpp
+++ 04-Collision/Mario.cpp
@@ -558,7 +558,7 @@ void CMario::Render()
 
 	animations->Get(aniId)->Render(x, y);
 
-	RenderBoundingBox();
+	//RenderBoundingBox();
 	
 	DebugOutTitle(L"Coins: %d", coin);
 	//DebugOutTitle(L"Score: %d", score);
---
+++ 05-SceneManager/Header.h
@@ -0,0 +1,18 @@
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_WORLDMAP 11020
+
+#define	COIN_WIDTH 10
+#define COIN_BBOX_WIDTH 10
+#define COIN_BBOX_HEIGHT 16
+
+class Cworldmap : public CGameObject {
+public:
+	Cworldmap(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	int IsBlocking() { return 0; }
+}; 
